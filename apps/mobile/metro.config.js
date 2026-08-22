3a2f771d9985febc031e4f9337ededd7a0e674d1
AzeezAbir <azeezabir28@gmail.com>
2026-08-22 11:00:15 +0530

    Fix Metro monorepo resolution on EAS by configuring watchFolders

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -5,6 +5,15 @@ const path = require('path');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Watch the monorepo root so Metro can resolve packages/shared
+const workspaceRoot = path.resolve(__dirname, '../..');
+config.watchFolders = [workspaceRoot];
+// Let Metro resolve packages using the root node_modules
+config.resolver.nodeModulesPaths = [
+  path.resolve(__dirname, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
 module.exports = withNativeWind(config, {
   input: path.resolve(__dirname, 'global.css'),
   configPath: path.resolve(__dirname, 'tailwind.config.js'),
